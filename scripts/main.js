258cb71984f463f8411e9e7c2fad23e973b28c3b
Luis Diez <luisjodiez@outlook.com>
2023-03-14 11:40:13 +0100

    Cleaner code, speak speed increase and control simple rolls

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,15 @@
-/*
-
-TO-DO:
-
-- Define correct format for the output strings [Done]
-- Test google speech library [WIP]
-- Feed the speech library with the output string
-- Include the speech library inside foundry
-
-*/
-
 Hooks.on('diceSoNiceRollStart', (nulo, doc) => {
   var faces = 0; // Dice number of faces
   var group_score = 0; // Score for the die grouped by faces
   var group_text = ""; // List of individual rolls for a group
 
-  // Replace operators for the string to be read by text to speech (spanish hardcode)
-  function replaceOperators(text) {
-    text = text.replace("*","por");
-    text = text.replace("/","entre");
-    text = text.replace("+","más");
-    text = text.replace("-","menos");
-    return text;
-  };
-
   // Iterate and parse individual rolls
   function parseTerms(term) {
     if (term.faces) {
       faces = term.faces;
-      term.values.forEach(parseIndividualRoll);
+      if (term.values.length > 1) {
+        term.values.forEach(parseIndividualRoll);
+      };
     };
   };
 
@@ -37,6 +19,7 @@ Hooks.on('diceSoNiceRollStart', (nulo, doc) => {
     var total = "Total: " + roll.total;
     var msg = new SpeechSynthesisUtterance(total);
     msg.lang = "es";
+    msg.rate = 1.5;
     window.speechSynthesis.speak(msg);
     // console.log(total);
   };
@@ -49,6 +32,7 @@ Hooks.on('diceSoNiceRollStart', (nulo, doc) => {
       var tirada = "Tirada individual: " + group_text + "."
       var msg = new SpeechSynthesisUtterance(tirada);
       msg.lang = "es";
+      msg.rate = 1.5;
       window.speechSynthesis.speak(msg);
       // console.log(tirada);
       group_score = 0;
